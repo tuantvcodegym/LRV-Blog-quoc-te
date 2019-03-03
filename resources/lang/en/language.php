ee5480c93e716532a37bde465ad760496c3b98d7
= <=>
2019-03-03 21:50:11 +0700

    file

---
+++ resources/lang/en/language.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TuanTran
+ * Date: 3/3/2019
+ * Time: 9:21 PM
+ */
+return [
+    'blog_application'=>'Blog Application',
+    'home' => 'Home',
+    'list_post' => 'Post list',
+    'create_post' => 'Create Post',
+    'save' => 'Save',
+    'post_not_exist' => 'Post not exist',
+    'title' => 'Title',
+    'content' => 'Content',
+];
---
+++ resources/lang/vi/language.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TuanTran
+ * Date: 3/3/2019
+ * Time: 9:22 PM
+ */
+return [
+  'blog_application' => 'Ung dung Blog',
+    'home' => 'Trang Chu',
+    'list_post' => 'Danh Sách Bài Viết',
+    'create_post' => 'Tạo Bài Viết',
+    'save' => 'Lưu',
+    'post_not_exist' => 'Không có bài viết nào',
+    'title' => 'Tiêu đề',
+    'content' => 'Nội dung',
+];
